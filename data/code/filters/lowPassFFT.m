654105c1eb9e0df0a4bc4e9c07b6898bb39cbc21
davis-timothy <davis.timo@husky.neu.edu>
2026-06-15 23:54:37 -0400

    Update lowPassFFT.m

--- data/code/filters/lowPassFFT.m
+++ data/code/filters/lowPassFFT.m
@@ -1,1 +1,20 @@
+function filtered = lowPassFFT(img,radius)
 
+[M,N] = size(img);
+
+F = fftshift(fft2(img));
+
+[X,Y] = meshgrid(1:N,1:M);
+
+centerX = N/2;
+centerY = M/2;
+
+D = sqrt((X-centerX).^2 + (Y-centerY).^2);
+
+H = D <= radius;
+
+Ffiltered = F .* H;
+
+filtered = real(ifft2(ifftshift(Ffiltered)));
+
+end
