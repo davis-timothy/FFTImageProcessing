a8351b2d9f042f5540207ce008bd21d9ea4da223
davis-timothy <davis.timo@husky.neu.edu>
2026-06-16 00:23:01 -0400

    Update loadImage.m

--- data/code/preprocessing/loadImage.m
+++ data/code/preprocessing/loadImage.m
@@ -2,12 +2,15 @@ function img = loadImage(filename)
 
 img = imread(filename);
 
+% Convert RGB to grayscale if needed
 if size(img,3) == 3
     img = rgb2gray(img);
 end
 
+% Standardize size
 img = imresize(img,[256 256]);
 
+% Convert to double
 img = double(img);
 
 end
